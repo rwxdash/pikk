ea10d161ac46abd4a687b0384f36e55ea5832c15
Oguzhan Ozdemir <aoozdemir@live.com>
2018-04-13 13:38:46 +0300

    add table printing

--- lib/pikk/main.rb
+++ lib/pikk/main.rb
@@ -1,3 +1,5 @@
+require 'terminal-table/import'
+
 module Pikk
   class << self
     def single_select(list = [{}])
@@ -11,11 +13,11 @@ module Pikk
 
     def pool(list = [{}], iteration: 50, pp: false)
       raise ArgumentError, 'Input is empty!' if list == [{}]
-      @iteration = iteration
+      $iteration = iteration
 
       arr = calculate_prob(list)
 
-      @iteration.times do
+      $iteration.times do
         selected_object = pick_from(arr)
         selected_object[:count] += 1
       end
@@ -51,18 +53,22 @@ module Pikk
     def to_table(list = [{}])
       list.sort_by { |h| h[:weight] }.reverse!
 
-      puts "\nNAME\tWEIGHT\tCOUNT\tPROBABILITY\tQUOTIENT (C/(P*I))"
-      puts "=" * 58
-
       fsum = 0
 
-      list.each do |f|
-        puts "#{f[:name]}\t#{f[:weight]}\t#{f[:count]}\t#{f[:probability]}\t\t#{f[:count].to_f/(f[:probability] * @iteration)}"
-        fsum += f[:count].to_f/(f[:probability] * @iteration)
+      ftable = table do
+        self.headings = "Name", "Weight", "Count", "Probability", "(C/(P*I))"
+
+        list.each do |f|
+          add_row [f[:name], f[:weight], f[:count], f[:probability], (f[:count].to_f/(f[:probability] * $iteration))]
+
+          fsum += f[:count].to_f/(f[:probability] * $iteration)
+        end
+        add_separator
+        add_row ['Total', { :value => (1 - fsum / list.count).abs, :colspan => 4, :alignment => :right }]
+        align_column 1, :center
       end
 
-      puts "\n"
-      puts (1 - fsum / list.count).abs
+      puts ftable
     end
   end
 end
